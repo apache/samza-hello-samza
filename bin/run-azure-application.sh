edc3ba5bedd334df8aa75f085091720e84df9f5c
Daniel Chen <29577458+dxichen@users.noreply.github.com>
2017-12-10 17:17:54 -0800

    Fixed run shel script

--- bin/run-azure-application.sh
+++ bin/run-azure-application.sh
@@ -27,4 +27,4 @@ mkdir -p $EXECUTION_PLAN_DIR
 
 [[ $JAVA_OPTS != *-Dlog4j.configuration* ]] && export JAVA_OPTS="$JAVA_OPTS -Dlog4j.configuration=file:$(dirname $0)/log4j-console.xml"
 
-exec $(dirname $0)/run-class.sh samza.examples.azure.application.AzureZKLocalApplication --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path=file://$PWD/deploy/samza/config/azure-application-local-runner.properties
+exec $(dirname $0)/run-class.sh samza.examples.azure.AzureZKLocalApplication --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path=file://$PWD/deploy/samza/config/azure-application-local-runner.properties
